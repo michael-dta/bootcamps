296958f74c6ac27045d87dfde0a41907b7a12312
michaeldta <michael.dta1@gmail.com>
2022-07-31 12:56:58 -0300

    Updated iac-apache.sh

--- linux-experience/3. Configurando Servidores Linux/4. IaC - Script de Provisionamento de um Servidor Web (Apache)/iac-apache.sh
+++ linux-experience/3. Configurando Servidores Linux/4. IaC - Script de Provisionamento de um Servidor Web (Apache)/iac-apache.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-# Atribuindo variáveis
-DIR_SITE=linux-site-dio-main
-DIR_APACHE=/var/www/html
-
 echo "[*] Atualizando o sistema operacional..."
 
 apt update
 apt upgrade -y
 
-echo "[*] Instalando o servidor web apache..."
+echo -e "\n[*] Instalando o servidor web apache..."
 
 apt install apache2 -y
 
-echo "[*] Instalando o Unzip..."
+echo -e "\n[*] Instalando o Unzip..."
 
 apt install unzip -y
 
-echo "[*] Download do site de exemplo..."
+echo -e "\n[*] Download do site de exemplo..."
 
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -O /tmp/main.zip
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
-echo "[*] Descompactando os arquivos e excluindo o zip..."
+echo -e "\n[*] Descompactando os arquivos e excluindo o zip..."
 
-unzip /tmp/main.zip /tmp/
-rm /tmp/main.zip
+unzip main.zip
+rm main.zip
 
-echo "[*] Excluindo página web padrão do apache..."
+echo -e "\n[*] Excluindo página web padrão do apache..."
 
-rm $DIR_APACHE/index.html
+rm /var/www/html/index.html
 
-echo "[*] Copiando os arquivos do site de exemplo para o diretório padrão do apache..."
+echo -e "\n[*] Copiando os arquivos do site de exemplo para o diretório padrão do apache..."
 
-cp /tmp/$DIR_SITE/* $DIR_APACHE
+cp ./linux-site-dio-main/* /var/www/html/
 
-echo "[!] Script IaC servidor Web Apache foi finalizado com sucesso."
+echo -e "\n[!] Script IaC servidor Web Apache foi finalizado com sucesso."
